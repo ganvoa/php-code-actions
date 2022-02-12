63fbeab55649e3cbc0db6165bc70b7e9affc9cab
ngamboa <ngamboa@insacom.cl>
2022-02-11 21:14:25 -0300

    refactor

--- src/actions/addConstructorAction.ts
+++ src/actions/addConstructorAction.ts
@@ -1,8 +1,21 @@
 import * as vscode from 'vscode';
+import ClassInspector from '../application/ClassInspector';
+import ConstructorCreator from '../application/ConstructorCreator';
+import PropertyCreator from '../application/PropertyCreator';
+import RegexpHelper from '../application/RegexpHelper';
+import Property from '../domain/Property';
+import { PropertyVisibility } from '../domain/PropertyVisibility';
+import { VsCodeEnvironment } from '../infrastructure/VsCodeEnvironment';
 
 export const addConstructorAction = (editor: vscode.TextEditor) => {
-    const properties: Map<string, PropertyClass> = getProperties(editor.document);
-    const position: vscode.Position = getPositionForConstructor(editor.document);
+
+    const regexpHelper = new RegexpHelper();
+    const propertyCreator = new PropertyCreator();
+    const vsCode = new VsCodeEnvironment();
+    const constructorCreator = new ConstructorCreator(propertyCreator, vsCode);
+    const classInspector = new ClassInspector(vsCode, regexpHelper);
+    const properties = classInspector.getProperties();
+    const offset = classInspector.getOffsetForConstructor();
 
     vscode.window.showQuickPick(
         Array.from(properties.values())
@@ -10,12 +23,12 @@ export const addConstructorAction = (editor: vscode.TextEditor) => {
             .map(prop => prop.name),
         {
             canPickMany: true,
-            title: 'Add Getter for'
+            title: 'Add Constructor for'
         }).then((selectedProperties?: string[]) => {
 
             if (undefined === selectedProperties) { return; }
 
-            const selectedAsArrayOfProperties: PropertyClass[] = [];
+            const selectedAsArrayOfProperties: Property[] = [];
             selectedProperties.forEach(p => {
                 let property = properties.get(p);
                 if (undefined !== property) {
@@ -23,11 +36,11 @@ export const addConstructorAction = (editor: vscode.TextEditor) => {
                 }
             });
 
-            const constructor = buildConstructor(selectedAsArrayOfProperties);
+            const constructor = constructorCreator.build(selectedAsArrayOfProperties);
 
             editor.insertSnippet(
                 new vscode.SnippetString(constructor),
-                position
+                editor.document.positionAt(offset.value)
             );
         });
 
